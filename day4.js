ed0460cfff5ebb5337dae61f38dbddf43c5eed80
T. Mohanraj <141510309+mohanraj172@users.noreply.github.com>
2024-04-10 20:53:22 +0530

    Update day4.js

--- day4.js
+++ day4.js
@@ -1,4 +1,4 @@
-   //object edit and delete
+ //object edit and delete
 
 let user = {
   name: 'mohan'
@@ -11,12 +11,13 @@ delete user.name//delete the user
 console.log(user)
 
 //array usind add and delete method...
+
 let user = ['mohan','user','age','void']
 console.log(user[1])// add inddex value of Array
 user.push = ('add')//add the element array
 user.pop()//delete the element array.
 
-user.splice(1,0,'apple') delecting are adding array middle element are delecting  the element exaple: splice(index,delete count,addlist)..
+user.splice(1,0,'apple') //delecting are adding array middle element are delecting  the element exaple: splice(index,delete count,addlist)..
 
 console.log(user)
 
@@ -26,17 +27,14 @@ console.log('mohan')
 }
 name()
 name()
-create a function it make the many to to call the function.
-
+//create a function it make the many to to call the function.
 
 function add(a, b){
 console.log (a+b)
 }
 add(2,3)
 add(50,20)
-
-let => it is unable to use the out side the function..
-
+//let => it is unable to use the out side the function..
 // create a function to calculate the
      
   function sqr(n){
