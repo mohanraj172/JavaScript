73a53b6b6e8e224df74f33d50b8385341250edba
T. Mohanraj <141510309+mohanraj172@users.noreply.github.com>
2024-04-10 20:49:58 +0530

    Add files via upload

---
+++ day4.js
@@ -0,0 +1,53 @@
+   //object edit and delete
+
+let user = {
+  name: 'mohan'
+  age: 20;
+  salary:60000
+  address:'pollachi'
+}
+user.email="email"//add the email
+delete user.name//delete the user
+console.log(user)
+
+//array usind add and delete method...
+let user = ['mohan','user','age','void']
+console.log(user[1])// add inddex value of Array
+user.push = ('add')//add the element array
+user.pop()//delete the element array.
+
+user.splice(1,0,'apple') delecting are adding array middle element are delecting  the element exaple: splice(index,delete count,addlist)..
+
+console.log(user)
+
+// function...
+function name() {
+console.log('mohan')
+}
+name()
+name()
+create a function it make the many to to call the function.
+
+
+function add(a, b){
+console.log (a+b)
+}
+add(2,3)
+add(50,20)
+
+let => it is unable to use the out side the function..
+
+// create a function to calculate the
+     
+  function sqr(n){
+  return n*n;
+  }            
+ let result= sqr(3)
+ console.log(result)
+
+//arrow function...
+
+cube=(n)=>{
+console.log(n*n*n)
+}
+cube(3)
